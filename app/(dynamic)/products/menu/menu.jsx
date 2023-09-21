c1bcb6c9561a1326bed1c6b3416424bde4ffd2d2
Soufiane Amama <soufianeamama70@gmail.com>
2023-09-21 13:05:19 +0100

    edit menu

--- app/(dynamic)/products/menu/menu.jsx
+++ app/(dynamic)/products/menu/menu.jsx
@@ -20,11 +20,11 @@ async function getData() {
 
   return (
     <div className="bg-[#374151] text-gray-200 flex justify-center items-center flex-row h-fit py-5 px-4 mx-auto">
-    <ul className="flex-row mx-auto gap-3 justify-around flex flex-wrap space-x-5 lg:flex">
-      <li className="flex text-sm w-fit hover:text-pink-400 bg-orange-300"><Link href={"/products"}>All</Link></li>
+    <ul className="flex-row mx-2 gap-3 justify-around flex flex-wrap space-x-5 lg:flex">
+      <li className="flex text-sm w-fit hover:text-pink-400"><Link href={"/products"}>All</Link></li>
       {
           categories.map(category => (
-            <li key={categories.indexOf(category)}  className="flex text-sm w-fit hover:text-pink-400 bg-orange-300"><Link href={`/products/category/${category}`}>{category}</Link></li>
+            <li key={categories.indexOf(category)}  className="flex text-sm w-fit hover:text-pink-400"><Link href={`/products/category/${category}`}>{category}</Link></li>
           ))
       }
     </ul> 
